9b872b26a5b1b3706480d87504d6822c37b61649
RyanLamingCCH <124586110+RyanLamingCCH@users.noreply.github.com>
2023-02-07 10:54:52 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -12,13 +12,10 @@
         var patient = smart.patient;
         var pt = patient.read();
         var obv = smart.patient.api.fetchAll({
-                    type: 'Observation',
-                    query: {
-                      code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
-                      }
+                    "type": 'Observation',
+                    "query": {
+						"code": 'http://loinc.org|8302-2,http://loinc.org|2085-9,http://loinc.org|2089-1,http://loinc.org|85354-9'
+						"date": 'gt2010-03-04'
                     }
                   });
 
@@ -37,8 +34,8 @@
           }
 
           var height = byCodes('8302-2');
-          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-          var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
+          var systolicbp = getBloodPressureValue(byCodes('85354-9'),'8480-6');
+          var diastolicbp = getBloodPressureValue(byCodes('85354-9'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
 
